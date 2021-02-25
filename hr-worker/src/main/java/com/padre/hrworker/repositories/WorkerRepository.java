039859173b64a9e7faa48650da7a8c390237fc3d
luispadre <52799375+luispadre@users.noreply.github.com>
2021-02-25 17:20:31 -0600

    Eliminacion

--- hr-worker/src/main/java/com/padre/hrworker/repositories/WorkerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.padre.hrworker.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.padre.hrworker.entities.Worker;
-
-public interface WorkerRepository extends JpaRepository<Worker,Long>{
-
-}
--- hr-worker/src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO tb_worker (name,daily_Income) VALUES ('Luis', 2212.2);
-INSERT INTO tb_worker (name,daily_Income) VALUES ('Jose', 2232.2);
