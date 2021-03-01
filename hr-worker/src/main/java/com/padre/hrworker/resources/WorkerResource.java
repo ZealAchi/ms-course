eafa37e6a558523627086f14f63b6e163f1c2556
luispadre <52799375+luispadre@users.noreply.github.com>
2021-03-01 17:41:06 -0600

    Hystrix para tolerancia a fallas

--- hr-worker/src/main/java/com/padre/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/padre/hrworker/resources/WorkerResource.java
@@ -33,6 +33,13 @@ public class WorkerResource {
 	}
 	@GetMapping(value="/{id}")
 	public ResponseEntity<Worker>findById(@PathVariable Long id){
+		
+//		try {
+//			Thread.sleep(3000L);
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+		
 		logger.info("PORT = "+env.getProperty("local.server.port"));
 		Worker obj=repository.findById(id).get();
 		return ResponseEntity.ok(obj);
