e3c3da275a2e60aa4001eb8afdccfa0d23df874c
luispadre <52799375+luispadre@users.noreply.github.com>
2021-03-02 16:44:25 -0600

    Project hr-user created

---
+++ hr-user/src/main/java/com/padre/hruser/HrUserApplication.java
@@ -0,0 +1,12 @@
+package com.padre.hruser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrUserApplication {
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(HrUserApplication.class, args);
+	}
+
+}
