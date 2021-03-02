d2b4b18a554046f36b6bbeed0cc82f8a14c976f7
luispadre <52799375+luispadre@users.noreply.github.com>
2021-03-02 11:53:36 -0600

    Project hr-config-server created

---
+++ hr-config-server/src/main/java/com/padre/hrconfigserver/HrConfigServerApplication.java
@@ -0,0 +1,12 @@
+package com.padre.hrconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrConfigServerApplication {
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(HrConfigServerApplication.class, args);
+	}
+
+}
