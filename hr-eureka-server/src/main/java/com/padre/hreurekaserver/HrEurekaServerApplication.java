ce6755e9e9d46a09bdf812585e280cb55b256166
luispadre <52799375+luispadre@users.noreply.github.com>
2021-03-05 10:50:52 -0600

    User, Role, many to many association

--- hr-eureka-server/src/main/java/com/padre/hreurekaserver/HrEurekaServerApplication.java
+++ hr-eureka-server/src/main/java/com/padre/hreurekaserver/HrEurekaServerApplication.java
@@ -2,10 +2,11 @@ package com.padre.hreurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class HrEurekaServerApplication {
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(HrEurekaServerApplication.class, args);
