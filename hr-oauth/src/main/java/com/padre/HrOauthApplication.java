27860a1c4740970289c63e19e362c1841781b4ab
luispadre <52799375+luispadre@users.noreply.github.com>
2021-03-08 12:46:54 -0600

    UserFeignClient

--- hr-oauth/src/main/java/com/padre/HrOauthApplication.java
+++ hr-oauth/src/main/java/com/padre/HrOauthApplication.java
@@ -2,8 +2,13 @@ package com.padre;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@EnableFeignClients
+@EnableEurekaClient
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class HrOauthApplication {
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(HrOauthApplication.class, args);
---
+++ hr-oauth/src/main/java/com/padre/services/UserService.java
@@ -0,0 +1,26 @@
+package com.padre.services;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.padre.entities.User;
+import com.padre.feignclients.UserFeignClient;
+
+@Service
+public class UserService {
+	private static Logger logger=LoggerFactory.getLogger(UserService.class);
+
+	@Autowired
+	private UserFeignClient userFeignClient;
+	public User findByEmail(String email) {
+		User user=userFeignClient.findByEmail(email).getBody();
+		if(user==null) {
+			logger.error("Email  not found "+ email);
+			throw new IllegalArgumentException("Email not found");
+		}
+		logger.info("Email found: "+email);
+		return user;
+	}
+}
